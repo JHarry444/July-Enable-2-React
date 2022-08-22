41896318bb825ad94bbfa9daccf58d34ad782879
jordan <jharrison1@qa.com>
2022-08-22 12:27:48 +0100

    added component example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './Footer';
 
 function App() {
+
+  const header = <h1>It's reactin' time!</h1>;
+  
+  const headFunc = () => <h1>It's reactin' time!</h1>;
   return (
     <div className="App">
-      <h1>It's reactin' time!</h1>
+      {header}
+      {headFunc()}
+      <main>
+        Content goes there
+      </main>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+return (
+    <>
+        <footer>I'm a footer!</footer>
+        <footer>I am also a footer</footer> 
+    </>
+);
+}
+
+export default Footer;
