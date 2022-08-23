aac632cab036e4385bde645188862e97c7f8ee64
jordan <jharrison1@qa.com>
2022-08-23 09:45:40 +0100

    added props and state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Footer from './Footer';
+import Trainer from './Trainer';
+import Converter from './Converter';
 
 function App() {
 
   const header = <h1>It's reactin' time!</h1>;
   
-  const headFunc = () => <h1>It's reactin' time!</h1>;
+  const HeadFunc = () => <h1>It's reactin' time!</h1>;
   return (
     <div className="App">
       {header}
-      {headFunc()}
+      {HeadFunc()}
+      <HeadFunc/>
       <main>
-        Content goes there
+        <Trainer name={4494} age={28} specialism="Software"/>
+        <Trainer name="Reece" age={25} specialism="Devops"/>
+        <section>
+          <Converter/>
+        </section>
       </main>
+
       <Footer/>
     </div>
   );
