3ffb84c760e6da52fed4ee2e523413605ec1c081
jordan <jharrison1@qa.com>
2022-08-25 10:03:42 +0100

    oops

--- src/components/DataRequests.jsx
+++ src/components/DataRequests.jsx
@@ -4,15 +4,21 @@ import Cake from './Cake';
 
 function DataRequests() {
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:4494/cakes/getAllCakes')
+    axios.get(`http://localhost:4494/cakes/findCakes?name=${search}`)
       .then((res) => setData(res.data))
       .catch((err) => console.error(err));
-  }, []); // componentDidMount
+  }, [search]); // componentDidMount + new value for search
 
   return (
     <>
+      <label htmlFor="cakeSearch">
+        Name:
+        <br />
+        <input type="text" id="cakeSearch" value={search} onChange={(e) => setSearch(e.target.value)} />
+      </label>
       {
       data.map(({ name, amount, cost }) => (
         <Cake
